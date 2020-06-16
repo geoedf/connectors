1cda21cc769bbea95209d174e01b2d6fbff691b5
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2020-06-16 08:24:23 -0400

    test various merge commit options (#6)

--- nasainput/setup.py
+++ nasainput/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='nasainput',
       version='0.1',
-      description='Connector for accessing NASA DAAC datasets)',
+      description='Connector for accessing NASA DAAC datasets',
       url='http://github.com/geoedf/connectors',
       author='Rajesh Kalyanam',
       author_email='rkalyanapurdue@gmail.com',
