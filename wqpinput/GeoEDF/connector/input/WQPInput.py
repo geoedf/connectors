8e0d171270fe2faec352e83bcca539b81de7df46
Jack Smith <smith1106@marshall.edu>
2020-08-17 20:04:01 -0500

    Syncing with WQPInput on GeoEDF plus a few changes (#11)
    * Init
    
    * Added start/end dates filter and switched to WQP site_id
    
    * Added sample YML workflow file
    
    * made more like NASAInput and moved YML file to subdirectory
    
    * move example YAML file to sudbdirectory
    
    Co-authored-by: Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>

--- wqpinput/GeoEDF/connector/input/WQPInput.py
+++ wqpinput/GeoEDF/connector/input/WQPInput.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from geoedfframework.utils.GeoEDFError import GeoEDFError
 from geoedfframework.GeoEDFPlugin import GeoEDFPlugin
 
 import requests
 import os
 
+""" Module for implementing the WQP input connector plugin. WQP (Water Quality Portal)
+    is a public web service (REST API) operated by the US EPA and USGS.
+    This module will implement the get() method required for all input plugins.
+"""
+
 class WQPInput(GeoEDFPlugin):
 
     base_url = "https://www.waterqualitydata.us/data"
     target_path = "data"
-    # no optional params yet, but keep around for future extension
+ 
     __optional_params = ['start_date','end_date']
     __required_params = ['site_id']
 
@@ -41,18 +49,24 @@ class WQPInput(GeoEDFPlugin):
     # if error, raise exception; if not, return True
 
     def get(self):
+        # set defaults if none provided
         if (self.start_date == None):
             self.start_date = ''
         if (self.end_date == None):
             self.end_date = '05-01-2020'
 
+        # build URL for REST API call
         wqp_url = self.base_url+"/Result/search?siteid="+self.site_id+"&StartDateLo="+self.start_date+"&StartDateHi="+self.end_date+"&mimeType=csv"
 
         try:
+            # do REST API GET call
             results = requests.get(url=wqp_url, stream=True)
+            # target_path is (re)set by the connector input instantiation
             out_path = '%s/%s.csv' % (self.target_path,self.site_id)
             with open(out_path,'wb') as out_file:
                 for chunk in results.iter_content(chunk_size=1024*1024):
                     out_file.write(chunk)
         except GeoEDFError:
+            raise
+        except:
             raise
--- wqpinput/setup.py
+++ wqpinput/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='wqpinput',
       version='0.1',
       description='Connector for EPA/USGA Water Quality Portal',
-      url='http://github.com/jacks9/wqpinput',
+      url='http://github.com/geoedf/connectors',
       author='Jack Smith',
       author_email='smith1106@marshall.edu',
       license='MIT',
