4f5e763144be18456f9b71c9a6050c25bfdeaad7
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2020-09-06 21:37:48 -0400

    FAOInput uses dataset codes instead of names

--- faoinput/setup.py
+++ faoinput/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='faoconnector',
       version='0.1',
-      description='Connector for U.N. FAOSTAT datasets)',
+      description='Connector for U.N. FAOSTAT datasets',
       url='http://github.com/geoedf/faoinput',
       author='Rajesh Kalyanam',
       author_email='rkalyanapurdue@gmail.com',
