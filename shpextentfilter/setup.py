e0b3628a1252d5fef303b2c92dab94ddb284d9fb
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2022-06-22 16:38:15 -0400

    Update setup.py

--- shpextentfilter/setup.py
+++ shpextentfilter/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shpextentfilter',
-      version='0.3',
+      version='0.4',
       description='Filter for returning the lat-lon extents of given shapefile',
       url='http://github.com/geoedf/shpextentfilter',
       author='Rajesh Kalyanam',
