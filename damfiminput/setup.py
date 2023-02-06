e8b4b1f5550b0732a3d79ac8a3a9047a237320f0
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-02-06 09:31:27 -0500

    Update setup.py

--- damfiminput/setup.py
+++ damfiminput/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='damfiminput',
-      version='0.2',
+      version='0.3',
       description='Connector for accessing Dam Flood Inundation Map datasets',
       url='http://github.com/geoedf/connectors',
       author='Rajesh Kalyanam',
