39c8f46e7370aa652cf7ddd1124b43c48ce28ea6
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-02-05 19:59:15 -0500

    Update setup.py

--- cuahsisubsetterinput/setup.py
+++ cuahsisubsetterinput/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cuahsisubsetterinput',
-      version='0.2',
+      version='0.3',
       description='Connector for running the CUAHSI subsetter for a given (list of) HUC12 IDs',
       url='http://github.com/geoedf/cuahsisubsetterinput',
       author='Rajesh Kalyanam',
